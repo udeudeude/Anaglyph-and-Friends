5b657457723a8d670422fe27bfabd5a349441814
udeudeude <udeudeude@gmail.com>
2026-09-23 22:41:03 -0400

    Fix production TypeScript build

--- frontend/src/PolarizedProjection.tsx
+++ frontend/src/PolarizedProjection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import type { StereoPairDraft } from './studioAssets'
 import './styles/PolarizedProjection.css'
 
